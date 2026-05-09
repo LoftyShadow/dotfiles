dc7933739b45564590bd94a8abddd2fadecf0dd4
聂明智 <1094290505@qq.com>
2026-05-09 19:56:55 +0800

    Improve Neovim bufferline contrast

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,42 @@ return {
     opts = {
       transparent_background = true,
     },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if not (vim.g.colors_name or ""):find("catppuccin") then
+            return
+          end
+
+          local colors = require("catppuccin.palettes").get_palette("frappe")
+          opts.highlights = require("catppuccin.special.bufferline").get_theme({
+            custom = {
+              frappe = {
+                buffer_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                numbers_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                close_button_selected = { fg = colors.crust, bg = colors.blue },
+                indicator_selected = { fg = colors.peach, bg = colors.blue, style = { "bold" } },
+                modified_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                separator_selected = { fg = colors.blue, bg = colors.blue },
+                duplicate_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                diagnostic_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                diagnostic_visible = { fg = colors.overlay1, bg = "NONE" },
+                hint_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                hint_diagnostic_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                info_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                info_diagnostic_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                warning_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                warning_diagnostic_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                error_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                error_diagnostic_selected = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+              },
+            },
+          })
+        end,
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
